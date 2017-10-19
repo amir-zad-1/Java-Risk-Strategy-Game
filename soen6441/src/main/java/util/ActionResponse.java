d6d69c5e0e54958c6cbe717d16720fdceb9362fd
jesus <jonofre21@gmail.com>
2017-10-19 11:40:30 -0400

    ActionResult documented

--- soen6441/src/main/java/util/ActionResponse.java
+++ soen6441/src/main/java/util/ActionResponse.java
@@ -1,18 +1,41 @@
 package util;
 
+
+/**
+ * To return multiple results from certain methods.
+ */
 public class ActionResponse {
 
     private boolean ok = false;
     private String message;
 
 
+    /**
+     * constructor which does nothing
+     */
     public ActionResponse()
     {
     }
+
+    /**
+     * sets message of the object
+     * @param message desired message to be delivered
+     */
+
+    /**
+     * Constructor
+     * @param message initial value of message property
+     */
     public ActionResponse(String message)
     {
         this.message = message;
     }
+
+    /**
+     * Constructor
+     * @param ok initial value of ok property
+     * @param message initial value of message property
+     */
     public ActionResponse(boolean ok, String message)
     {
         this.ok = ok;
@@ -20,10 +43,28 @@ public class ActionResponse {
     }
 
 
+    /**
+     * return values of ok
+     * @return if action is done or not
+     */
     public boolean getOk() {return this.ok;}
+
+    /**
+     * set the ok property value
+     * @param ok indicated if the action is done or not
+     */
     public void setOk(boolean ok) {this.ok=ok;}
 
+    /**
+     * return value of message property
+     * @return message value
+     */
     public String getMessage(){ return this.message; }
+
+    /**
+     * sets message value
+     * @param message value of message
+     */
     public void setMessage(String message){ this.message = message; }
 
 
