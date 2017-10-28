3c30c48c581786c2f0a8c12684bd7d9a52f4c63f
Manohar-Gunturu <gunturumanohar2@gmail.com>
2017-10-27 23:41:41 -0400

    Implemented PhaseView UI

--- soen6441/src/main/java/view/PhaseView.java
+++ soen6441/src/main/java/view/PhaseView.java
@@ -1,1 +1,45 @@
-class PhaseView{}
+/**
+ * 
+ */
+package view;
+
+import java.util.Observable;
+import java.util.Observer;
+
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import model.Territory;
+
+/**
+ * @author SA
+ *
+ */
+public class PhaseView implements IView,Observer{
+
+	Label label = new Label("Player one");
+	
+	/* (non-Javadoc)
+	 * @see view.IView#getView()
+	 */
+	@Override
+	public Scene getView() {		
+        label.setStyle("-fx-font-family: 'Saira Semi Condensed', sans-serif;");
+		Scene scene = new Scene(label);
+		scene.getStylesheets().add("https://fonts.googleapis.com/css?family=Saira+Semi+Condensed");
+		return scene;
+	}
+
+	/* (non-Javadoc)
+	 * @see java.util.Observer#update(java.util.Observable, java.lang.Object)
+	 */
+	@Override
+	public void update(Observable arg0, Object arg1) {
+		// TODO Auto-generated method stub
+        Territory t = (Territory) arg0; 
+        
+        label.setText(t.getCurrentPlayer());
+	}
+
+	
+	
+}
