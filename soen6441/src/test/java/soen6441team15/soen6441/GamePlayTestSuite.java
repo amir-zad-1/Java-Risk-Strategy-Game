35448d4ddd3e6b2dfb363c632671fcefdc47fa3b
Amir Mohsen Hossein Zadeh <am1hosseinzadeh@gmail.com>
2017-11-08 10:34:47 -0500

    31 test cases

--- soen6441/src/test/java/soen6441team15/soen6441/GamePlayTestSuite.java
+++ soen6441/src/test/java/soen6441team15/soen6441/GamePlayTestSuite.java
@@ -13,6 +13,8 @@ import org.junit.runner.RunWith;
                       GpTest7.class,
                       GpTest8.class,
                       GpTest9.class,
+                      MapIOTest.class,
+                      MapValidation.class
                     })
 
 
